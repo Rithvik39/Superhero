6043f65f3b7626c65a949783a952487217ee8afe
Rithvik39 <72606950+Rithvik39@users.noreply.github.com>
2021-03-06 12:20:11 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,134 @@
+canvas= new fabric.Canvas("myCanvas");
+var player_x= 10;
+var player_y= 10;
+block_image_height= 30;
+block_image_width= 30;
+var player_image= "";
+var block_img_object= "";
+
+function player_update(){
+    fabric.Image.fromURL("player.png", function(IMG){
+        player_image= IMG;
+        player_image.scaleToWidth(145);
+        player_image.scaleToHeight(135);
+        player_image.set({
+            top:player_y,
+            left:player_x
+        });
+        canvas.add(player_image);
+    });
+}
+
+function block_img_update(get_img){
+fabric.Image.fromURL(get_img, function(IMG){
+    block_img_object= IMG;
+    block_img_object.scaleToWidth(block_image_width);
+    block_img_object.scaleToHeight(block_image_height);
+    block_img_object.set({
+        top:player_y,
+        left:player_x
+    });
+    canvas.add(block_img_object);
+});
+}
+
+window.addEventListener("keydown", my_keydown);
+function my_keydown(e){
+var key_pressed= e.keyCode;
+console.log(key_pressed);
+
+if(e.shiftKey == true && key_pressed == "80"){
+    console.log("Shift and P key is pressed");
+    block_image_width = block_image_width + 10;
+    block_image_height = block_image_height + 10;
+    document.getElementById("current_width").innerHTML = block_image_width;
+    document.getElementById("current_height").innerHTML = block_image_height;
+}
+
+if(e.shiftKey == true && key_pressed == "77"){
+    console.log("Shift and M key is pressed");
+    block_image_width = block_image_width - 10;
+    block_image_height = block_image_height - 10;
+    document.getElementById("current_width").innerHTML = block_image_width;
+    document.getElementById("current_height").innerHTML = block_image_height;
+}
+
+if(key_pressed == "38"){
+    console.log("Up arrow key is pressed");
+    Up();
+}
+if(key_pressed == "40"){
+    console.log("Down arrow key is pressed");
+    Down();
+}
+if(key_pressed == "37"){
+    console.log("Left arrow key is pressed");
+    Left();
+}
+if(key_pressed == "39"){
+    console.log("right arrow key is pressed");
+    Right();
+}
+
+
+if(key_pressed == "70"){
+    console.log("F key is pressed");
+    block_img_update("ironman_face.png");
+}
+if(key_pressed == "66"){
+    console.log("B key is pressed");
+    block_img_update("spiderman_body.png");
+}
+if(key_pressed == "76"){
+    console.log("L key is pressed");
+    block_img_update("hulk_legs.png");
+}
+if(key_pressed == "82"){
+    console.log("R key is pressed");
+    block_img_update("thor_right_hand.png");
+}
+if(key_pressed == "72"){
+    console.log("H key is pressed");
+    block_img_update("captain_america_left_hand.png");
+}
+}
+
+function Up(){
+    if(player_y >= 0){
+        player_y = player_y - block_image_height;
+        console.log("block image height =" + block_image_height);
+        console.log("when up arrow key is pressed, x = " + player_x + "y = " + player_y);
+        canvas.remove(player_image);
+        player_update();
+    }
+}
+
+function Down(){
+    if(player_y <= 650){
+        player_y = player_y + block_image_height;
+        console.log("block image height =" + block_image_height);
+        console.log("when down arrow key is pressed, x = " + player_x + "y = " + player_y);
+        canvas.remove(player_image);
+        player_update();
+    }
+}
+
+function Left(){
+    if(player_x >= 0){
+        player_x = player_x - block_image_width;
+        console.log("block image width =" + block_image_width);
+        console.log("when left arrow key is pressed, x = " + player_x + "y = " + player_y);
+        canvas.remove(player_image);
+        player_update();
+    }
+}
+
+function Right(){
+    if(player_x <= 850){
+        player_x = player_x + block_image_width;
+        console.log("block image width =" + block_image_width);
+        console.log("when right arrow key is pressed, x = " + player_x + "y = " + player_y);
+        canvas.remove(player_image);
+        player_update();
+    }
+}
